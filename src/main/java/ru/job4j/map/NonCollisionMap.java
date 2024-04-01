c54d81614c4da6dc6047ce23d82de5f403bc0f1b
AnKondratev <kondratev.an@inbox.ru>
2024-04-02 00:31:20 +0900

    Реализовать собственную структуру данных - HashMap

--- src/main/java/ru/job4j/map/NonCollisionMap.java
+++ src/main/java/ru/job4j/map/NonCollisionMap.java
@@ -14,21 +14,6 @@ public class NonCollisionMap<K, V> implements SimpleMap<K, V> {
 
     private MapEntry<K, V>[] table = new MapEntry[capacity];
 
-    @Override
-    public boolean put(K key, V value) {
-        if (((double) count / capacity) >= LOAD_FACTOR) {
-            expand();
-        }
-        boolean result = false;
-        if (table[indexFor(hash(Objects.hashCode(key)))] == null) {
-            table[indexFor(hash(Objects.hashCode(key)))] = new MapEntry<>(key, value);
-            count++;
-            modCount++;
-            result = true;
-        }
-        return result;
-    }
-
     private int hash(int hashCode) {
         return hashCode ^ (hashCode >>> 16);
     }
@@ -37,24 +22,46 @@ public class NonCollisionMap<K, V> implements SimpleMap<K, V> {
         return hash % capacity;
     }
 
+    private int index(K key) {
+        return indexFor(hash(Objects.hashCode(key)));
+    }
+
+    @Override
+    public boolean put(K key, V value) {
+        double countf = count;
+        if ((countf / capacity) >= LOAD_FACTOR) {
+            expand();
+        }
+        boolean result = false;
+        if (table[index(key)] == null) {
+            table[index(key)] = new MapEntry<>(key, value);
+            count++;
+            modCount++;
+            result = true;
+        }
+        return result;
+    }
+
     private void expand() {
-        capacity = capacity == 0 ? 1 : capacity * 2;
+        capacity = capacity * 2;
         var newTable = new MapEntry[capacity];
         Arrays.stream(table).
                 filter(Objects::nonNull).
-                forEach(e -> newTable[indexFor(hash(Objects.
-                        hashCode(e.key)))] = e);
+                forEach(e -> newTable[index(e.key)] = e);
         table = newTable;
+    }
+
+    private boolean keyEqualityCheck(K key) {
+        return (key == null && table[index(key)].key == null)
+                || (key != null && key.equals(table[index(key)].key));
     }
 
     @Override
     public V get(K key) {
-        int index = indexFor(hash(Objects.hashCode(key)));
         V result = null;
-        if (table[index] != null) {
-            if ((key == null && table[index].key == null)
-                    || (key != null && key.equals(table[index].key))) {
-                result = table[index].value;
+        if (table[index(key)] != null) {
+            if (keyEqualityCheck(key)) {
+                result = table[index(key)].value;
             }
         }
         return result;
@@ -62,12 +69,10 @@ public class NonCollisionMap<K, V> implements SimpleMap<K, V> {
 
     @Override
     public boolean remove(K key) {
-        int index = indexFor(hash(Objects.hashCode(key)));
         boolean result = false;
-        if (table[index] != null) {
-            if ((key == null && table[index].key == null)
-                    || (key != null && key.equals(table[index].key))) {
-                table[index] = null;
+        if (table[index(key)] != null) {
+            if (keyEqualityCheck(key)) {
+                table[index(key)] = null;
                 count--;
                 modCount++;
                 result = true;
