31a77d14657906fca9fda3c5910248287f808fd2
AnKondratev <kondratev.an@inbox.ru>
2024-04-02 19:16:50 +0900

    Реализовать собственную структуру данных - HashMap

--- src/main/java/ru/job4j/map/NonCollisionMap.java
+++ src/main/java/ru/job4j/map/NonCollisionMap.java
@@ -28,7 +28,7 @@ public class NonCollisionMap<K, V> implements SimpleMap<K, V> {
 
     @Override
     public boolean put(K key, V value) {
-        if ((count * 1.0 / capacity) >= LOAD_FACTOR) {
+        if (capacity - (count / LOAD_FACTOR)  <= 0) {
             expand();
         }
         int index = index(key);
