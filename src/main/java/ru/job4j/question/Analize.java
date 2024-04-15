322e1dabb54184a2a4e64ff46a328e39d691ff46
AnKondratev <kondratev.an@inbox.ru>
2024-04-15 18:42:57 +0900

    Статистика по коллекции

--- src/main/java/ru/job4j/question/Analize.java
+++ src/main/java/ru/job4j/question/Analize.java
@@ -1,29 +1,33 @@
 package ru.job4j.question;
 
-import java.util.Map;
+import java.util.HashMap;
 import java.util.Set;
-import java.util.stream.Collectors;
-
-import static java.lang.StrictMath.toIntExact;
 
 public class Analize {
 
     public static Info diff(Set<User> previous, Set<User> current) {
-        Map<Integer, String> previousMap = previous.stream()
-                .collect(Collectors.toMap(User::getId, User::getName));
-        Map<Integer, String> currentMap = current.stream()
-                .collect(Collectors.toMap(User::getId, User::getName));
+        int added = 0;
+        int changed = 0;
+        int deleted = 0;
+        HashMap<Integer, String> previousMap = new HashMap<>();
+        previous.forEach(user -> previousMap.put(user.getId(), user.getName()));
+        HashMap<Integer, String> currentMap = new HashMap<>();
+        current.forEach(user -> currentMap.put(user.getId(), user.getName()));
 
-        int added = toIntExact(current.stream()
-                .filter(user -> !previousMap.containsKey(user.getId()))
-                .count());
-        int changed = toIntExact(previous.stream()
-                .filter(user -> !user.getName()
-                        .equals(currentMap.getOrDefault(user.getId(), user.getName())))
-                .count());
-        int deleted = toIntExact(previous.stream()
-                .filter(user -> !currentMap.containsKey(user.getId()))
-                .count());
+        for (User user : current) {
+            if (!previousMap.containsKey(user.getId())) {
+                added++;
+            }
+        }
+
+        for (User user : previous) {
+            if (!user.getName().equals(currentMap.getOrDefault(user.getId(), user.getName()))) {
+                changed++;
+            }
+            if (!currentMap.containsKey(user.getId())) {
+                deleted++;
+            }
+        }
 
         return new Info(added, changed, deleted);
     }
