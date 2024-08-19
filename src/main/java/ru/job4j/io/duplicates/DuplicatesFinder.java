85a33f005dcce285c462c62f0e714be9dac831f1
AnKondratev <kondratev.an@inbox.ru>
2024-08-19 03:52:55 +0300

    Поиск дубликатов

--- src/main/java/ru/job4j/io/duplicates/DuplicatesFinder.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesFinder.java
@@ -3,48 +3,14 @@ package ru.job4j.io.duplicates;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 public class DuplicatesFinder {
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get("D:/Photo");
-        findDups(start);
-    }
+        DuplicatesVisitor vis = new DuplicatesVisitor();
+        Files.walkFileTree(Path.of("D:/Photo"), vis);
+        vis.getDuplicates().forEach(System.out::println);
 
-    public static void findDups(Path root) throws IOException {
-        DuplicatesVisitor dupFinder = new DuplicatesVisitor();
-        Files.walkFileTree(root, dupFinder);
-        Map<String, FileProperty> duplicatesFiles = dupFinder.getAllFiles();
-        List<String> filesToRemove = getStrings(duplicatesFiles);
 
-        for (String keyToRemove : filesToRemove) {
-            duplicatesFiles.remove(keyToRemove);
-        }
-        for (Map.Entry<String, FileProperty> entry : duplicatesFiles.entrySet()) {
-            System.out.println("File: " + entry.getKey() + ", Size: " + entry.getValue().getSize());
-        }
-
-    }
-
-    private static List<String> getStrings(Map<String, FileProperty> duplicatesFiles) {
-        Map<String, Integer> valueCounts = new HashMap<>();
-        List<String> filesToRemove = new ArrayList<>();
-
-        for (Map.Entry<String, FileProperty> entry : duplicatesFiles.entrySet()) {
-            String val = entry.getValue().getName() + entry.getValue().getSize();
-            valueCounts.put(val, valueCounts.getOrDefault(val, 0) + 1);
-        }
-
-        for (Map.Entry<String, FileProperty> entry : duplicatesFiles.entrySet()) {
-            String val = entry.getValue().getName() + entry.getValue().getSize();
-            if (valueCounts.get(val) == 1) {
-                filesToRemove.add(entry.getKey());
-            }
-        }
-        return filesToRemove;
     }
 }
+
--- src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
@@ -2,25 +2,34 @@ package ru.job4j.io.duplicates;
 
 import java.io.IOException;
 import java.nio.file.FileVisitResult;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
-
-    private Map<String, FileProperty> allFiles = new HashMap<>();
+    private List<String> duplicates = new ArrayList<>();
+    private Map<String, List<Path>> fileMap = new HashMap<>();
 
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attributes) throws IOException {
-        BasicFileAttributes info = Files.readAttributes(file, BasicFileAttributes.class);
-        allFiles.put(file.toString(), new FileProperty(info.size(), file.getFileName().toString()));
+        String key = attributes.size() + file.getFileName().toString();
+        fileMap.putIfAbsent(key, new ArrayList<>());
+        fileMap.get(key).add(file);
+        if (fileMap.get(key).size() > 1) {
+            for (Path duplicate : fileMap.get(key)) {
+                if (!duplicates.contains(duplicate.toAbsolutePath().toString())) {
+                    duplicates.add(duplicate.toAbsolutePath().toString());
+                }
+            }
+        }
         return FileVisitResult.CONTINUE;
     }
 
-    public Map<String, FileProperty> getAllFiles() {
-        return allFiles;
+    public List<String> getDuplicates() {
+        return duplicates;
     }
 }
