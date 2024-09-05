1554bee0e08184863c38d0cef82e15718f2b7bbf
andrey <kondratev.an@inbox.ru>
2024-09-05 20:33:45 +0900

    12. Валидация параметров запуска [#246865]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -1,7 +1,5 @@
 package ru.job4j.io;
 
-import com.sun.jdi.connect.Connector;
-
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -11,9 +9,6 @@ import java.util.function.Predicate;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        if (args.length == 0) {
-            throw new IllegalArgumentException("Root folder is null. Usage ROOT_FOLDER.");
-        }
         validation(args);
         Path start = Paths.get(args[0]);
         search(start, path -> path.toFile().getName().endsWith(args[1])).forEach(System.out::println);
@@ -26,8 +21,15 @@ public class Search {
     }
 
     private static void validation(String[] args) {
-        if (args.length < 2) {
-            throw new IllegalArgumentException("Root folder is null. Usage ROOT_FOLDER.");
+        if (args.length != 2) {
+            throw new IllegalArgumentException("Error: Expected 2 arguments: ROOT_FOLDER and FILE_EXTENSION.");
+        }
+        Path rootPath = Paths.get(args[0]);
+        if (!Files.isDirectory(rootPath)) {
+            throw new IllegalArgumentException("Error: the specified path '" + args[0] + "' is not a directory.");
+        }
+        if (!args[1].startsWith(".")) {
+            throw new IllegalArgumentException("Error: the file extension must start with a dot.");
         }
     }
 }
